afd1ce66365d42d376f85cc0e320a7d35e468828
gbktosin <104964198+gbktosin@users.noreply.github.com>
2022-09-17 16:54:45 -0400

    Added "Flix" files to repository

---
+++ Pods/Target Support Files/Pods-UnitFooTests/Pods-UnitFooTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UnitFooTests : NSObject
+@end
+@implementation PodsDummy_Pods_UnitFooTests
+@end
---
+++ UnitFoo/TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewCell.swift
+//  UnitFoo
+//
+//  Created by Jesse Oseafiana on 9/17/22.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
